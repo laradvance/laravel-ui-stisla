f60717ef73795b54a8b5322f344e18c4b6261738
Muhlis Abdi <muhlisabdi@gmail.com>
2020-06-19 17:42:36 +0800

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -23,7 +23,7 @@
                 <li><a class="nav-link" href="">Top Navigation</a></li>
             </ul>
         </li>
-        <li class=active><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+        <li class=active><a class="nav-link" href=""><i class="far fa-square"></i> <span>Blank Page</span></a></li>
         <li class="dropdown">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
             <ul class="dropdown-menu">
