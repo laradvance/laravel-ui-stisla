bc5be1cb0b995d3961b8a870362d6eeba8c49df5
Muhlis Abdi <muhlisabdi@gmail.com>
2020-06-19 17:35:30 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer-left">
-  Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+  Copyright &copy; {{ env('APP_NAME') }} {{ date('Y') }}
 </div>
 <div class="footer-right">
-  2.3.0
+  {{ App::VERSION() }}
 </div>
